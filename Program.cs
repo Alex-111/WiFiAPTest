971d4fd1b1fe757d2e457b76a9b067fb2f596de0
kerschal <kerschl.alexander@kermi.de>
2023-07-28 10:46:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿
+using Iot.Device.DhcpServer;
+using nanoFramework.Runtime.Native;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace WifiAP
+{
+    public class Program
+    {
+        // Start Simple WebServer
+        static WebServerSimple server = new WebServerSimple();
+
+        // Connected Station count
+        static int connectedCount = 0;
+
+        // GPIO pin used to put device into AP set-up mode
+        const int SETUP_PIN = 5;
+
+        static IPAddress localIP = IPAddress.Parse(WirelessAP.SoftApIP);
+
+        public static void Main()
+        {
+            
+            Wireless80211.Disable();
+            
+            if (WirelessAP.Setup() == false)
+            {
+                // Reboot device to Activate Access Point on restart
+                Debug.WriteLine($"Setup Soft AP, Rebooting device");
+                Power.RebootDevice();
+            }
+          
+            var dhcpserver = new DhcpServer
+            {
+                CaptivePortalUrl = $"http://{WirelessAP.SoftApIP}"
+            };
+            var dhcpInitResult = dhcpserver.Start(localIP, new IPAddress(new byte[] { 255, 255, 255, 0 }));
+            if (!dhcpInitResult)
+            {
+                Debug.WriteLine($"Error initializing DHCP server.");
+            }
+
+            Debug.WriteLine($"Running Soft AP, waiting for client to connect");
+           
+        
+            NetworkChange.NetworkAPStationChanged += NetworkChange_NetworkAPStationChanged;
+          
+            server.Start(localIP);
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+      
+
+     
+        private static void NetworkChange_NetworkAPStationChanged(int NetworkIndex, NetworkAPStationEventArgs e)
+        {
+           
+            try
+            {
+                Debug.WriteLine($"NetworkAPStationChanged event Index:{NetworkIndex} Connected:{e.IsConnected} Station:{e.StationIndex} ");
+                Debug.WriteLine($"Soft AP IP address :{WirelessAP.GetIP()}");
+
+                if (e.IsConnected)
+                {
+                    WirelessAPConfiguration wapconf = WirelessAPConfiguration.GetAllWirelessAPConfigurations()[0];
+                    WirelessAPStation station = wapconf.GetConnectedStations(e.StationIndex);
+
+                    string macString = BitConverter.ToString(station.MacAddress);
+                    Debug.WriteLine($"Station mac {macString} Rssi:{station.Rssi} PhyMode:{station.PhyModes} ");
+
+                }
+              
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.ToString());
+            }
+
+
+        }
+    }
+}
---
+++ WebServerSimple.cs
@@ -0,0 +1,163 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections;
+using System.Device.Gpio;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using nanoFramework.Runtime.Native;
+
+namespace WifiAP
+{
+    public class WebServerSimple
+    {
+        HttpListener _listener;
+        Thread _serverThread;
+
+        public void Start(IPAddress localIP)
+        {
+            if (_listener == null)
+            {
+                _listener = new HttpListener("http", 80, localIP);
+                _serverThread = new Thread(RunServer);
+                _serverThread.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            if (_listener != null)
+            {
+                _listener.Stop();
+                _listener = null;
+            }
+        }
+        private void RunServer()
+        {
+            _listener.Start();
+
+            while (_listener.IsListening)
+            {
+                try
+                {
+                    Debug.WriteLine("listener.GetContext()");
+                    var context = _listener.GetContext();
+                    if (context != null)
+                        ProcessRequest(context);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                }
+
+            }
+            _listener.Close();
+
+            _listener = null;
+        }
+
+        string resp = CreateMainPage("testpage");
+        private void ProcessRequest(HttpListenerContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+            string responseString;
+            
+
+            Debug.WriteLine("Request: "+ request.RawUrl);
+
+            switch (request.HttpMethod)
+            {
+                case "GET":
+                    string[] url = request.RawUrl.Split('?');
+                    if (url[0] == "/")
+                    {
+                        response.ContentType = "text/html";
+                        response.StatusCode = 200;
+                        OutPutResponse(response, resp);
+
+                    }
+                    else
+                    {
+                        response.ContentType = "text/html";
+                        response.StatusCode = 404;
+                        OutPutResponse(response, resp);
+                    }
+                    break;
+                 
+            }
+
+            response.Close();
+ 
+        }
+
+       
+
+        static void OutPutResponse(HttpListenerResponse response, string responseString)
+        {
+            var responseBytes = System.Text.Encoding.UTF8.GetBytes(responseString);
+            OutPutByteResponse(response, System.Text.Encoding.UTF8.GetBytes(responseString));
+        }
+        static void OutPutByteResponse(HttpListenerResponse response, Byte[] responseBytes)
+        {
+            response.ContentLength64 = responseBytes.Length;
+            response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+
+        }
+
+        static Hashtable ParseParamsFromStream(Stream inputStream)
+        {
+            byte[] buffer = new byte[inputStream.Length];
+            inputStream.Read(buffer, 0, (int)inputStream.Length);
+
+            return ParseParams(System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+
+        static Hashtable ParseParams(string rawParams)
+        {
+            Hashtable hash = new Hashtable();
+
+            string[] parPairs = rawParams.Split('&');
+            foreach (string pair in parPairs)
+            {
+                string[] nameValue = pair.Split('=');
+                hash.Add(nameValue[0], nameValue[1]);
+            }
+
+            return hash;
+        }
+        static string CreateMainPage(string message)
+        {
+
+            return $"<!DOCTYPE html><html>{GetCss()}<body>" +
+                    "<h1>NanoFramework</h1>" +
+                    "<form method='POST'>" +
+                    "<fieldset><legend>Wireless configuration</legend>" +
+                    "Ssid:</br><input type='input' name='ssid' value='' ></br>" +
+                    "Password:</br><input type='password' name='password' value='' >" +
+                    "<br><br>" +
+                    "<input type='submit' value='Save'>" +
+                    "</fieldset>" +
+                    "<b>" + message + "</b>" +
+                    "</form></body></html>";
+        }
+
+        static string GetCss()
+        {
+            return "<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><style>" +
+                "*{box-sizing: border-box}" +
+                "h1,legend {text-align:center;}" +
+                "form {max-width: 250px;margin: 10px auto 0 auto;}" +
+                "fieldset {border-radius: 5px;box-shadow: 3px 3px 15px hsl(0, 0%, 90%);font-size: large;}" +
+                "input {width: 100%;padding: 4px;margin-bottom: 8px;border: 1px solid hsl(0, 0%, 50%);border-radius: 3px;font-size: medium;}" +
+                "input[type=submit]:hover {cursor: pointer;background-color: hsl(0, 0%, 90%);transition: 0.5s;}" +
+                " @media only screen and (max-width: 768px) { form {max-width: 100%;}} " +
+                "</style><title>NanoFramework</title></head>";
+        }
+    }
+}
